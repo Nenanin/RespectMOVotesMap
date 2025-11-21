834cad34eedcf1a700deae3b59aaa5e9c19b9b36
Nenanin <126510839+Nenanin@users.noreply.github.com>
2025-11-21 13:36:18 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import TileLayer from 'ol/layer/Tile.js';
+import OSM from 'ol/source/OSM.js';
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    projection: 'EPSG:4326',
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
